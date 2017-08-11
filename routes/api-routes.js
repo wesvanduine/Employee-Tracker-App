cf101181b277f39f0cf91c3bddb169ad7081b7cc
nverges <nverges@email.arizona.edu>
2017-08-10 19:09:43 -0700

    trying to get routes working still

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -13,27 +13,80 @@ var db = require("../models");
 module.exports = function(app) {
 
 
-    // GET route for getting all of the posts
-    app.get("/employees", function(req, res) {
-        // var query = {};
-        // // if (req.query.author_id) {
-        // //     query.AuthorId = req.query.author_id;
-        // // }
-        // // Here we add an "include" property to our options in our findAll query
-        // // We set the value to an array of the models we want to include in a left outer join
-        // // In this case, just db.Author
-        // db.Post.findAll({
-        //     where: query,
-        //     include: [db.Employees]
-        // }).then(function(dbEmployees) {
-        //     res.json(dbEmployees);
-        // });
-
-        res.json(res.Employees);
-
-        console.log(res.Employees);
-
+    app.post('api/login', function(req, res) {
+        console.log(req.body.username);
+        console.log(req.body.password);
     });
+
+    app.get('/api', function(req, res) {
+        db.Employees.findAll({}).then(function(data) {
+            res.json(data);
+        });
+    });
+
+
+    // app.post('api/add-employees', function(req, res) {
+
+    //     alert('JKLAFDALJKDFS');
+
+    //     db.Employees.create({
+    //         first_name: req.body.first_name,
+    //         last_name: req.body.last_name,
+    //         email: req.body.email,
+    //         phone_number: req.body.phone_number, 
+    //         address: req.body.address,
+    //         username: req.body.username,
+    //         password: req.body.password,
+    //         admin: req.body.admin
+    //     }).then(function(data) {
+    //         res.redirect('/');
+    //         console.log(data);
+    //         alert('success!!!!!!!');
+    //         });
+
+    // });
+
+
+    // // GET route for getting all of the posts
+    // app.get("/employees", function(req, res) {
+    //     // var query = {};
+    //     // // if (req.query.author_id) {
+    //     // //     query.AuthorId = req.query.author_id;
+    //     // // }
+    //     // // Here we add an "include" property to our options in our findAll query
+    //     // // We set the value to an array of the models we want to include in a left outer join
+    //     // // In this case, just db.Author
+    //     // db.Post.findAll({
+    //     //     where: query,
+    //     //     include: [db.Employees]
+    //     // }).then(function(dbEmployees) {
+    //     //     res.json(dbEmployees);
+    //     // });
+
+    //     res.json(res.Employees);
+
+    //     console.log(db.Employees);
+
+    // });
+
+    // app.get("/", function(req, res) {
+
+    //     db.Employees.findAll({}).then(function(data) {
+    //         var hbsObject = {
+    //             Employees: data
+    //                 // Burger: data
+    //         };
+    //         console.log(hbsObject);
+    //         res.render("index", hbsObject);
+    //     });
+    // });
+
+
+
+
+    // app.get('/', function(req, res) {
+    //     res.render
+    // }
 
     // // Get rotue for retrieving a single post
     // app.get("/api/posts/:id", function(req, res) {
@@ -57,25 +110,12 @@ module.exports = function(app) {
     //     });
     // });
 
-    app.post('api/add-employee', function(req, res) {
 
-        db.Employees.create({
-            first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            email: req.body.email,
-            phone_number: req.body.phone_number, 
-            address: req.body.address,
-            username: req.body.username,
-            password: req.body.password,
-            admin: req.body.admin
-        }).then(function(data) {
-            res.redirect('/');
-            console.log(data);
-            alert('success!!!!!!!');
-        })
-    })
 
-    // // DELETE route for deleting posts
+
+
+
+    // DELETE route for deleting posts
     // app.delete("/api/posts/:id", function(req, res) {
     //     db.Post.destroy({
     //         where: {
